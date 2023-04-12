1cc469ea9ac75dd510ef6664fa83000da96345cb
Luiz Eduardo <gs.luizeduardo00@gmail.com>
2023-04-12 08:55:28 -0300

    feat: create base use cases

--- application/src/main/java/com/fullcycle/admin/catalog/application/UseCase.java
+++ application/src/main/java/com/fullcycle/admin/catalog/application/UseCase.java
@@ -2,8 +2,6 @@ package com.fullcycle.admin.catalog.application;
 
 import com.fullcycle.admin.catalog.domain.category.Category;
 
-public class UseCase {
-    public Category execute() {
-        return Category.newCategory("", "", true);
-    }
+public abstract class UseCase <IN, OUT> {
+    public abstract OUT execute(IN in);
 }
